4e3fe49308c1f6a4c3a9c08446cf44d67627c2ad
Hannah Lord <hlord@seas.upenn.edu>
2021-09-11 08:41:35 -0400

    backend folder created

--- backend/cluster_colors.py
+++ backend/cluster_colors.py
@@ -9,4 +9,4 @@ def cluster_colors(path):
   image_slic = seg.slic(image,n_segments=3000)
 
   # label2rgb replaces each discrete label with the average interior color
-  # image_show(color.label2rgb(image_slic, image, kind='avg'));
+  image_show(color.label2rgb(image_slic, image, kind='avg'));
